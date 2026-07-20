f1076397601c2895bcca55048698aff7ce7b2d7c
lubangakene-dev <2400716470@mubs.ac.ug>
2026-07-20 13:17:23 +0300

    Complete About Us page with responsive layout, school history, vision, mission, core values, facilities, and call-to-action

---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-19.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-5b40ff1c', {"layout":"number","heading":"Happy leaners","numberPrefix":"","numberSuffix":" +","startNumber":0,"endNumber":5000,"totalNumber":5000,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-ea6765f0', {"layout":"number","heading":"Qualified teachers ","numberPrefix":"","numberSuffix":" +","startNumber":0,"endNumber":80,"totalNumber":100,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-2afd2b53', {"layout":"number","heading":"Years of excellence","numberPrefix":"","numberSuffix":"","startNumber":0,"endNumber":20,"totalNumber":100,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-4a7c0363', {"layout":"number","heading":"Qualified teachers ","numberPrefix":"","numberSuffix":"% DIV 1 ","startNumber":0,"endNumber":30,"totalNumber":100,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+ });
