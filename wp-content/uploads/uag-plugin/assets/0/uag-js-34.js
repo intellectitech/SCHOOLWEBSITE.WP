ee6e2e8b7bdb499e48f253c3468cca0a234f2cee
lubangakene-dev <2400716470@mubs.ac.ug>
2026-07-21 17:07:32 +0300

    create Contact Us page

---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-34.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-c0ab5469' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-b485785b' );
+});
+window.addEventListener("DOMContentLoaded", function(){
+	UAGBForms.init( {"block_id":"141c3ba4","reCaptchaEnable":false,"reCaptchaType":"v2","reCaptchaSiteKeyV2":"","reCaptchaSiteKeyV3":"","afterSubmitToEmail":"duncanjoshua43@gmail.com","afterSubmitCcEmail":"","afterSubmitBccEmail":"","afterSubmitEmailSubject":"Form Submission","sendAfterSubmitEmail":true,"confirmationType":"message","hidereCaptchaBatch":false,"captchaMessage":"Please fill up the above captcha.","confirmationUrl":""}, '.uagb-block-141c3ba4', 34 );
+});
+ });
